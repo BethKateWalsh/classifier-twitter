3e0e4c16641d27de7f946485bcc4d5c3452d7945
Beth Walsh <bethwalsh91@gmail.com>
2018-08-10 21:46:55 +0100

    Twitter api details added to seperate file and .gitignore

--- data_collection/stream_tweets.py
+++ data_collection/stream_tweets.py
@@ -1,17 +1,10 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 import tweepy
+from twitter_api import ACCESS_TOKEN
+from twitter_api import ACCESS_SECRET
+from twitter_api import CONSUMER_KEY
+from twitter_api import CONSUMER_SECRET
 
-description = ""
-loc = ""
-text = ""
-coords = ""
-name = ""
-user_created = ""
-followers = ""
-id_str = ""
-created = ""
-retweets = ""
-bg_color = ""
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
@@ -25,18 +18,7 @@ class StreamListener(tweepy.StreamListener):
     def on_status(self, status):
         if status.retweeted_status:
             return
-        description = status.user.description
-        loc = status.user.location
-        text = status.text
-        coords = status.coordinates
-        name = status.user.screen_name
-        user_created = status.user.created_at
-        followers = status.user.followers_count
-        id_str = status.id_str
-        created = status.created_at
-        retweets = status.retweet_count
-        bg_color = status.user.profile_background_color
-        print(description)
+        print(status.text)
 
     def on_error(self, status_code):
         if status_code == 420:
