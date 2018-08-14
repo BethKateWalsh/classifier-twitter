a18e4ae4db0dadca739ca18e3463495534e40449
Beth Walsh <bethwalsh91@gmail.com>
2018-08-14 09:11:43 +0100

    Dropped laptop

--- test/test-mongo.py
+++ test/test-mongo.py
@@ -1,8 +1,9 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 from pymongo import MongoClient
 from atlasclusterpassword import atlasstring
-# connect to MongoDB, change the << MONGODB URL >> to reflect your
-# own connection string
-mongostring = atlasstring
 
-client = MongoClient(mongostring)
+client = MongoClient(atlasstring)
+
+db = client.azuresupport
+
+print(client.mflix)
