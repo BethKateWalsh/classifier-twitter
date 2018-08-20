dc11e17d35428a593431ef3e7a60fe16b5ebfd94
“BethKateWalsh” <bethwalsh91@gmail.comssh-keygen -t rsa -C bethwalsh91@gmail.comgit config --global color.ui truegit config --global user.name BethKateWalshgit config --global user.email bethwalsh91@gmail.com>
2018-08-20 21:17:50 +0100

    Atlas Connection Mad

--- data_collection/stream_tweets.py
+++ data_collection/stream_tweets.py
@@ -1,30 +1,25 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
-import tweepy
 from twitter_api import ACCESS_TOKEN
 from twitter_api import ACCESS_SECRET
 from twitter_api import CONSUMER_KEY
 from twitter_api import CONSUMER_SECRET
+import pymongo
+from pymongo import MongoClient
+import json
+import tweepy
+from pprint import pprint
 
+
+# Connect to Twitter API
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-# This what will stream the tweets from the Twiiter API.
 
+# Connect to database MongoDB
 
-class StreamListener(tweepy.StreamListener):
-
-    def on_status(self, status):
-        if status.retweeted_status:
-            return
-        print(status.text)
-
-    def on_error(self, status_code):
-        if status_code == 420:
-            return False
-
-
-stream_listener = StreamListener()
-stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-stream.filter(track=["@AzureSupport"])
+client = MongoClient()
+db = client.tweet_db
+tweet_collection = db.tweet_collection
+tweet_collection.create_index([("id", pymongo.ASCENDING)], unique=True)
