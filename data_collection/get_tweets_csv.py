683f6ae9ad5b4fd2ddb2effdb95a5641d32a3835
Beth Walsh <bethwalsh@MacBook-Pro-2.local>
2018-08-23 22:44:01 +0100

    SQLITE database create with TABLE

--- data_collection/get_tweets_csv.py
+++ data_collection/get_tweets_csv.py
@@ -47,29 +47,30 @@ with open(filename, 'w', newline='') as f:
     thewriterobject.writerow(['id_user', 'id_str_user', 'name_user', 'screen_name_user', 'description_user', 'url_user', 'followers_count_user_user', 'favourites_count_user', 'lang_user', 'id_status', 'id_str_status', 'textstatus', 'created_at_status', 'truncated', 'in_reply_to_screen_name', 'retweet_count', 'favorite_count', 'retweeted', 'lang_status'])
 
     for tweet in searched_tweets:
-        id_user = tweet.user.id
-        id_str_user = tweet.user.id_str
-        name_user = tweet.user.name
-        screen_name_user = tweet.user.screen_name
-        location_user = tweet.user.location
-        description_user = tweet.user.description
-        url_user = tweet.user.url
-        followers_count_user_user = tweet.user.followers_count
-        favourites_count_user = tweet.user.favourites_count
-        lang_user = tweet.user.lang
-        # Fields from the status object
-        id_status = tweet.id
-        id_str_status = tweet.id_str
-        text_status = tweet.text
-        created_at_status = tweet.created_at
-        truncated = tweet.truncated
-        in_reply_to_screen_name = tweet.in_reply_to_screen_name
-        retweet_count = tweet.retweet_count
-        favorite_count = tweet.favorite_count
-        retweeted = tweet.retweeted
-        lang_status = tweet.lang
-        # Add 1000 tweets
-        thewriterobject.writerow([id_user, id_str_user, name_user, screen_name_user, description_user, url_user, followers_count_user_user, favourites_count_user, lang_user, id_status, id_str_status, text_status, created_at_status, truncated, in_reply_to_screen_name, retweet_count, favorite_count, retweeted, lang_status])
+        if tweet.user.screen_name != "azuresupport":
+            id_user = tweet.user.id
+            id_str_user = tweet.user.id_str
+            name_user = tweet.user.name
+            screen_name_user = tweet.user.screen_name
+            location_user = tweet.user.location
+            description_user = tweet.user.description
+            url_user = tweet.user.url
+            followers_count_user_user = tweet.user.followers_count
+            favourites_count_user = tweet.user.favourites_count
+            lang_user = tweet.user.lang
+            # Fields from the status object
+            id_status = tweet.id
+            id_str_status = tweet.id_str
+            text_status = tweet.text
+            created_at_status = tweet.created_at
+            truncated = tweet.truncated
+            in_reply_to_screen_name = tweet.in_reply_to_screen_name
+            retweet_count = tweet.retweet_count
+            favorite_count = tweet.favorite_count
+            retweeted = tweet.retweeted
+            lang_status = tweet.lang
+            # Add 1000 tweets
+            thewriterobject.writerow([id_user, id_str_user, name_user, screen_name_user, description_user, url_user, followers_count_user_user, favourites_count_user, lang_user, id_status, id_str_status, text_status, created_at_status, truncated, in_reply_to_screen_name, retweet_count, favorite_count, retweeted, lang_status])
 
 
 # Connect to database MongoDB
